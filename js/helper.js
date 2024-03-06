0f413ca28a451f477acd3123a0db45c022ece4b7
Lua <laisolizq@gmail.com>
2024-03-06 01:25:09 +0100

    First version main

--- js/helper.js
+++ js/helper.js
@@ -1,5 +1,8 @@
 const { F1Field } = require("ffjavascript");
 const rootsOfUnity4096 = require("./rootsOfUnity4096.json");
+const {
+    scalar2fea
+} = require("@0xpolygonhermez/zkevm-commonjs").smtUtils;
 
 module.exports = class myHelper {
     blobSize = 4096;
@@ -64,5 +67,61 @@ module.exports = class myHelper {
             }
         }
         throw new Error("Root not found");
+    }
+
+    eval_getLastL1InfoTreeIndex(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+    
+        return [ctx.Fr.e(ctx.input.lastL1InfoTreeIndex), ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero];
+    }
+
+    eval_getLastL1InfoTreeRoot(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+        return scalar2fea(ctx.Fr, Scalar.e(ctx.input.lastL1InfoTreeRoot));
+    }
+
+    eval_getTimestampLimit(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+    
+        return [ctx.Fr.e(ctx.input.timestampLimit), ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero];
+    }
+
+    eval_getZkGasLimit(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+    
+        return scalar2fea(ctx.Fr, Scalar.e(ctx.input.zkGasLimit));
+    }
+
+    eval_getType(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+    
+        return [ctx.Fr.e(ctx.input.type), ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero];
+    }
+
+    eval_getZ(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+        return scalar2fea(ctx.Fr, Scalar.e(ctx.input.z));
+    }
+
+    eval_getY(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+        return scalar2fea(ctx.Fr, Scalar.e(ctx.input.y));
+        //return [ctx.Fr.e(ctx.input.y), ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero];
+    }
+
+    eval_getBlobL2HashData(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+        return scalar2fea(ctx.Fr, Scalar.e(ctx.blobL2HashData));
+    }
+
+    eval_getForcedHashData(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+        return scalar2fea(ctx.Fr, Scalar.e(ctx.input.forcedHashData));
+    }
+
+    eval_getBlobLen(ctx, tag) {
+        if (tag.params.length != 0) throw new Error(`Invalid number of parameters (0 != ${tag.params.length}) function ${tag.funcName} ${ctx.sourceRef}`);
+        
+        return [ctx.Fr.e((ctx.input.blobData.length-2) / 2), ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero, ctx.Fr.zero];
     }
 };
